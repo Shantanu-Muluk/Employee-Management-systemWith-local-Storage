f615e405875f0d0856ec82a52c8f316b87a8798f
Shantanu-Muluk <shantanumuluk9878@gmail.com>
2024-12-15 21:13:01 +0530

    Add and retrive Functions done

---
+++ index.js
@@ -0,0 +1,51 @@
+
+let form = document.querySelector("form");
+let table = document.querySelector("table");
+
+form.addEventListener("submit",(event)=>{
+    // event.preventDefault();
+
+    let nameInput = document.querySelector("#name").value 
+    let emailInput = document.querySelector("#email").value
+    let phoneInput = document.querySelector("#phone").value
+    let jobInput = document.querySelector("#job").value
+
+    let savetoLocal = {
+        nameInput,
+        emailInput,
+        phoneInput,
+        jobInput
+    }
+
+    let emp = JSON.parse(localStorage.getItem("Employee Details: ")) || []
+
+    emp.push(savetoLocal)
+
+    localStorage.setItem("Employee Details: ", JSON.stringify(emp))
+
+    nameInput.value = ""
+    emailInput.value = ""
+    phoneInput.value = ""
+    jobInput.value = ""
+})
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    let emp = JSON.parse(localStorage.getItem("Employee Details: ")) || []
+
+    let tableBody = document.getElementById("employee-table-body")
+
+    emp.forEach(i=>{
+        let row = document.createElement("tr");
+        row.innerHTML = `<td>${i.nameInput}</td>
+                    <td>${i.emailInput}</td>
+                    <td>${i.phoneInput}</td>
+                    <td>${i.jobInput}</td>
+                    <td>
+                        <button class="edit-btn">✏️</button>
+                        <button class="delete-btn">❌</button>
+                    </td>`
+        tableBody.appendChild(row)            
+    })
+
+    
+})
